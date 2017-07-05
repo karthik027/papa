4bb4a039b5c69743204f38b5afd7d6e3ad6400a0
karthik027 <senthurmurugan3234@gmail.com>
2017-07-05 09:36:23 +0530

    Create Str.java

---
+++ Str.java
@@ -0,0 +1,13 @@
+# papa
+import java.io.*;
+import java.util.*;
+public class Str
+{
+public static void main(String args[])throws IOException
+{
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+String a=br.readLine();
+Integer n=Integer.parseInt(a);
+System.out.println(n);
+}
+}
